372619017cdfbab6be2599056a1ab8b323aced9b
Gazala M <gazlaws.dev@gmail.com>
2018-04-01 16:37:41 +0530

    Update correct reciever file

--- receiver.c
+++ receiver.c
@@ -1,13 +1,9 @@
-#include <sys/time.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <string.h>
-#define HOST "localhost"
-#include <sys/types.h>
-#include <netdb.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
 
 struct pkt {
 		int ACK;
@@ -15,93 +11,77 @@ struct pkt {
 		char data[25];
 };
 
-
-
 int main(int argc, char *argv[]){
-	int senderSocket,i,n;
-	char input[25];
-	struct timeval timeout={2,0}; //set timeout for 2 seconds
-	int recvlen;
-	struct sockaddr_in receiverAddr,senderAddr;	//clientAddr is new
+	int recvSocket,err;
+	struct pkt recvPacket,ackPacket;
+	struct sockaddr_in recieverAddr,senderAddr;
+	struct sockaddr_storage serverStorage;
 	socklen_t addr_size;
-	struct hostent *host;
+	int drop=0;
 	int counter=0;
 	
-	
-   
-    
-    if(argc != 3){
+	if(argc != 3){
 		printf("Usage : %s <IP> <PORT>\n", argv[0]);
 		return 0;
 	}
+
+	//welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
 	
-	host = gethostbyname(argv[1]);	//127.0.0.1
+	recvSocket = socket(AF_INET, SOCK_DGRAM, 0); //using diagram instead of stream-- UDP ?AF
 	
-	if (host == NULL) {
-        printf("ERROR, no such host as %s\n", HOST);
+	if (recvSocket < 0) 
+    	printf("ERROR opening socket\n");
+
+	recieverAddr.sin_family = AF_INET;	
+	recieverAddr.sin_port = htons(atoi(argv[2]));
+	recieverAddr.sin_addr.s_addr = inet_addr(argv[1]);
+	
+	memset(recieverAddr.sin_zero, '\0', sizeof recieverAddr.sin_zero);
+	
+	addr_size = sizeof senderAddr;
+	err = bind(recvSocket, (struct sockaddr*)&recieverAddr, addr_size);
+	
+	 if(err == -1)
+    {
+        perror("bind error: ");
         exit(0);
     }
-    
-	senderSocket = socket(AF_INET, SOCK_DGRAM, 0); //using diagram instead of stream-- UDP
-	
-	receiverAddr.sin_family = AF_INET;
-	receiverAddr.sin_port = htons(atoi(argv[2]));
-	receiverAddr.sin_addr = *((struct in_addr *)host->h_addr);
-	
-	memset(receiverAddr.sin_zero, '\0', sizeof receiverAddr.sin_zero);
-
-	addr_size = sizeof receiverAddr;
-	
-	
-	printf("\nEnter a string to send, q to quit\n ");
-
-	while(1)
-	{	int err;
-		struct pkt packet,recvPacket;
-		
-		scanf("%s",input);
-		
-		if(strcmp("q",input)==0){
-			printf("Quiting\n");
-			return 0;
-		}
-		
-		strcpy(packet.data,input);
-		packet.seqnum=counter;
-		
-		//int socket fd,     payload,  length,      flags,      "to"-destination IP and POrt, sizeof
-		sendto(senderSocket, &packet, sizeof(packet), 0, (struct sockaddr*)&receiverAddr,  addr_size);
-		
-		printf("Sent pkt with seq number %d\n",packet.seqnum);
+	while(1){
+		//wait for a packet
+		err = recvfrom(recvSocket, &recvPacket, sizeof(recvPacket), 0,(struct sockaddr*)&senderAddr, &addr_size);
+		if(err>0){
+			int drop = rand() % 10;
+			if(drop<2){
+				//drop pkt occasionally, for testing purposes
 				
-		/* set receive UDP message timeout */
-
-		setsockopt(senderSocket,SOL_SOCKET,SO_RCVTIMEO,(char*)&timeout,sizeof(struct timeval));
-
-		/* Receive UDP message */
-		recvlen =  recvfrom(senderSocket, &recvPacket, sizeof(recvPacket), 0,(struct sockaddr*)&receiverAddr, &addr_size);
-		while(1){
-			if (recvlen >= 0) {
-				//Message Received
-				if(recvPacket.ACK == counter+1){
-					printf("Ack received for %d\n\n",counter);
-					counter++;
-					break;
-				} else {	//won't be sent out of order, this is unneccesary
-					printf("Resending pkt %d:%s\n",packet.seqnum,packet.data);
-					sendto(senderSocket, &packet, sizeof(packet), 0, (struct sockaddr*)&receiverAddr,  addr_size);
-					recvlen = recvfrom(senderSocket, &recvPacket, sizeof(recvPacket), 0,(struct sockaddr*)&receiverAddr, &addr_size);
-				}
+				ackPacket.ACK=counter;	//last correctly recieved
+				printf("Dropping %s, %d\n",recvPacket.data,recvPacket.seqnum);
+				
+				continue;	//go back to wait for a packet (this one never reached)
+			}else if(drop<5){
+				//drop ack
+				counter;
+				printf("Correctly recieved: %s, with seq number :%d ack: %d, but the ack will drop\n",recvPacket.data,recvPacket.seqnum,counter);
+				continue;
+			} else if(recvPacket.seqnum==counter){
+				counter++;
+				ackPacket.ACK=counter;
+				printf("Correctly recieved: %s, with seq number :%d ack: %d\n",recvPacket.data,recvPacket.seqnum,counter);
+			} else if(recvPacket.seqnum<counter){
+				//duplicate old packet
+				printf("Duplicate %d, expecting %d\n",recvPacket.seqnum, counter);
+				ackPacket.ACK=counter;	//reACK
 			}
-			else{
-				//Message Receive Timeout			
-				printf("Timeout! Resending pkt %d:%s\n",packet.seqnum,packet.data);
-				sendto(senderSocket, &packet, sizeof(packet), 0, (struct sockaddr*)&receiverAddr,  addr_size);
-				recvlen = recvfrom(senderSocket, &recvPacket, sizeof(recvPacket), 0,(struct sockaddr*)&receiverAddr, &addr_size);
-
+			else {
+				ackPacket.ACK=counter;
+				printf("Out of order %d, expected %d\n",recvPacket.seqnum,counter);
 			}
+		
+			//int socket fd,     payload,  length,      flags,      "to"-destination IP and POrt, sizeof
+			sendto(recvSocket, &ackPacket, sizeof(ackPacket), 0, (struct sockaddr*)&senderAddr,  addr_size);
+		} else {
+			perror("recv failed\n");
 		}
 	}
-	return 0;
-	
+return 0;
 }
